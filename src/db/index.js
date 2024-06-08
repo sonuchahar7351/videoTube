ae9dca6db0ec2c49885270942b5f84d294394cd8
sonuchahar7351 <sonuchahar7351@gmail.com>
2024-06-08 15:12:22 +0530

    some changes in index.js

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const MONGO_URI=process.env.MONGO_URI;
 const connectDB=async ()=>{
       try {
             await mongoose.connect(`${MONGO_URI}/${DB_NAME}`);
-             console.log("Database connected");
+             console.log("Database connected successfully");
             
       } catch (error) {
             console.log("monogDB connection error ", error);
